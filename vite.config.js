489c12d3057b9ab57e477797f305886023e0f306
Sujit <sujeetnangare@gmail.com>
2025-08-17 11:01:48 +0530

    gh-pages added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "https://github.com/sujit6060/AI_Chat",
 });
